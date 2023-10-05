6955a15ac875c87d5f21d954236681c5f7a36ec4
Rouan van der Ende <rouan@8bo.org>
2023-10-05 11:42:32 +0200

    0.2.70

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { DetailedHTMLProps, ReactNode } from "react";
 import { cn } from "../utils/cn";
 
-export function Heading(props: { children: string; className?: string }) {
+export function Heading(
+  props: DetailedHTMLProps<
+    React.HTMLAttributes<HTMLHeadingElement>,
+    HTMLHeadingElement
+  >
+) {
   return (
     <h1
+      {...props}
       className={cn(
         "text-xl font-semibold uppercase text-neutral-600 dark:text-neutral-400",
         props.className
       )}
-    >
-      {props.children}
-    </h1>
+    />
   );
 }
